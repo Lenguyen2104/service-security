95de1dd591c201f7a6a5d0450efef09c7caabd42
Letannguyen <letannguyen210496@gmail.com>
2023-12-13 21:25:53 +0700

    Init project

---
+++ src/main/java/com/security/duanspringboot/DuanSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.security.duanspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuanSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuanSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/duanspringboot/DuanSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.duanspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuanSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
