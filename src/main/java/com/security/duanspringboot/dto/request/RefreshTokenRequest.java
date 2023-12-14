dd443bc5a6a7f8fcb9e5da3fca54ee4b33ba134c
Lê Tấn Nguyên <letannguyen@MacBook-Air-cua-Le-2.local>
2023-12-14 23:38:58 +0700

    api sigin and refresh

---
+++ src/main/java/com/security/duanspringboot/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,9 @@
+package com.security.duanspringboot.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RefreshTokenRequest {
+
+    private String token;
+}
--- src/main/java/com/security/duanspringboot/service/AuthenticationService.java
+++ src/main/java/com/security/duanspringboot/service/AuthenticationService.java
@@ -1,9 +1,16 @@
 package com.security.duanspringboot.service;
 
+import com.security.duanspringboot.dto.request.RefreshTokenRequest;
+import com.security.duanspringboot.dto.request.SignInRequest;
 import com.security.duanspringboot.dto.request.SignUpRequest;
+import com.security.duanspringboot.dto.response.JwtAuthenticationResponse;
 import com.security.duanspringboot.entity.UserModel;
 
 public interface AuthenticationService {
 
     UserModel signup(SignUpRequest signUpRequest);
+
+    JwtAuthenticationResponse signIn(SignInRequest signInRequest);
+
+    JwtAuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
 }
